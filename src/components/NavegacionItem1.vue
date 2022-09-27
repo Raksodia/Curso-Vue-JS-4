f2a79e1a71481445695f0941d2cddaad7fad128a
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 12:42:21 -0500

    Navegacion Item Objet
    Navegacion Item Objet

---
+++ src/components/NavegacionItem1.vue
@@ -0,0 +1,23 @@
+<template>
+    <li class="nav-item">
+        <a class="nav-link" v-bind:style="{ backgroundColor: colorRandom}" v-bind:class="{active:mostrarActivo}" aria-current="page" href="#">{{navegacionData.titulo}}</a>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'NavegacionItem1',
+    data (){
+        return {
+            mostrarActivo: true,
+            colorRandom : "#"+Math.floor(Math.random()*6777215).toString(16)
+        } 
+    },
+    props:{
+        navegacionData: Object
+    }
+}
+</script>
+
+<style>
+</style>
