79c898596229ba77aa84b1f6f42f5cf7f88accee
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 11:59:46 -0500

    Navegacion Item
    Navegacion Item

---
+++ src/components/NavegacionItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'NavegacionItem',
+    data (){
+        return {
+            
+        } 
+    }
+}
+</script>
+
+<style>
+</style>
