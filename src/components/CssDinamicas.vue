43f0babfe24727ab87f61574b49e2a0333ef8132
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 01:01:37 -0500

    CSS Dinamica
    CSS Dinamica

---
+++ src/components/CssDinamicas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="CssDinamicas"> 
+        <div v-bind:style="{ backgroundColor: colorRandom}">
+        CSS Dinamicas
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CssDinamicas',
+    data (){
+        return {
+             colorRandom : "#"+Math.floor(Math.random()*6777215).toString(16)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
