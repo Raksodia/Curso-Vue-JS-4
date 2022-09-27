199da838253c1d76a22eb61839a1eb0348c083bb
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 14:46:52 -0500

    Iterar Componentes y Datos Dinamicos en Bucles
    Iterar Componentes y Datos Dinamicos en Bucles

---
+++ src/components/NavegacionItem2.vue
@@ -0,0 +1,23 @@
+<template>
+    <li class="nav-item">
+        <a class="nav-link" v-bind:style="{ backgroundColor: colorRandom}" v-bind:class="{active:mostrarActivo}" aria-current="page" v-bind:href="navegacionData.url">{{navegacionData.titulo}}</a>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'NavegacionItem2',
+    data (){
+        return {
+            mostrarActivo: true,
+            colorRandom : "#"+Math.floor(Math.random()*6777215).toString(16)
+        } 
+    },
+    props:{
+        navegacionData: Object
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/NavegacionSecundaria.vue
@@ -0,0 +1,49 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+<a class="navbar-brand" href="#">Navbar</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        
+        <NavegacionItem2 v-bind:key="index" v-bind:navegacionData="item" v-for="(item,index) in ItemsNavegacion" />
+    </ul>
+</div>
+</div>
+ </nav>
+</template>
+
+<script>
+import NavegacionItem2 from './NavegacionItem2.vue' 
+
+export default {
+    name: 'NavegacionSecundaria',
+    components: {
+        NavegacionItem2
+    },
+    data (){ 
+        return {
+          ItemsNavegacion: [
+            {
+                titulo:"Inicio",
+                url:"/Inicio"
+            },
+            {
+                titulo:"Acerca",
+                url:"/Acerca"
+            },
+            {
+                titulo:"Tienda",
+                url:"/Tienda"
+            },
+            {
+                titulo:"Contacto",
+                url:"/Contacto"
+            }
+          ]
+        }
+    }
+}
+</script>
