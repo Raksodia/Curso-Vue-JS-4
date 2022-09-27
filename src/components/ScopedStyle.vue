218f4026a067007e372cdfc4ed42d96928563e66
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 01:13:48 -0500

    Scoped Style
    Scoped Style

---
+++ src/components/ScopedStyle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="texto"> 
+        Texto Azul
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ScopedStyle',
+    data (){
+        return {
+             
+        }
+    }
+}
+</script>
+
+<style>
+.texto {
+    color:blue;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 12px;;
+}
+</style>
