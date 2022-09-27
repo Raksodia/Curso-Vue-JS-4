7ae683fd33952e8bd27b9f10ac981e7ca7cd394f
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 12:09:25 -0500

    Navegacion Item
    Navegacion Item

--- src/components/NavegacionItem.vue
+++ src/components/NavegacionItem.vue
@@ -1,6 +1,6 @@
-<template>
+    <template>
     <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" v-bind:style="{ backgroundColor: colorRandom}" v-bind:class="{active:mostrarActivo}" aria-current="page" href="#">Link</a>
     </li>
 </template>
 
@@ -9,7 +9,8 @@ export default {
     name: 'NavegacionItem',
     data (){
         return {
-            
+            mostrarActivo: true,
+            colorRandom : "#"+Math.floor(Math.random()*6777215).toString(16)
         } 
     }
 }
