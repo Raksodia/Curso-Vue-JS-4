e001a9f27ac0491d6d3b564b2dcff2360732ff03
Williamns Robles <robles.williamns@gmail.com>
2022-09-26 19:04:40 -0500

    Mi Componente Funciona
    Mi Componente Funciona

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="MiComponente"> 
+        <h1>Mi Componente Funciona</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MiComponente',
+    data (){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
