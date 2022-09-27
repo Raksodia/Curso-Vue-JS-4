3ab63d809347e620299f0f7c1be54f0297d4b9a4
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 11:49:43 -0500

    Contenido
    Contenido

---
+++ src/components/ContenidoSitio.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="texto"> 
+        Texto Contenido Sitio
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContenidoSitio',
+    data (){
+        return {
+             
+        }
+    }
+}
+</script>
+
+<style scoped>
+.texto {
+    color:red;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 12px;;
+}
+</style>
