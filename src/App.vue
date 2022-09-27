0bb5e89666e7ab68babab0d1931dee131fcae0d9
Williamns Robles <robles.williamns@gmail.com>
2022-09-27 00:18:02 -0500

    Bootstrap
    Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
       <div class="container">
+        <div class="row">
+          <NavegacionPrincipal/>
+        </div>
         <MiComponente/>
         <DesplegarArreglo/>
         <DesplegarObjetos/>
@@ -15,14 +18,17 @@ import MiComponente from './components/MiComponente.vue'
 import DesplegarArreglo from './components/DesplegarArreglo.vue'
 import DesplegarObjetos from './components/DesplegarObjetos.vue'
 import ComponenteCondicional from './components/ComponenteCondicional.vue'
+import NavegacionPrincipal from './components/NavegacionPrincipal.vue'
 
 export default {
   name: 'App',
   components: {
+    NavegacionPrincipal,
     MiComponente,
     DesplegarArreglo,
     DesplegarObjetos,
-    ComponenteCondicional
+    ComponenteCondicional,
+   
   } 
 }
 </script>
