ee6303d1cb176893d5de78410fc634655d0f81c9
Williamns Robles <robles.williamns@gmail.com>
2022-09-28 16:20:30 -0500

    Opcion Button v-on
    Opcion Button v-on

---
+++ src/components/OpcionButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="texto"> 
+        <h3>Opcion 1 {{opcion}}</h3>
+        <button class="btn btn-primary" v-on:click="opcion='---CLICK---'">Opcion 1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OpcionButton',
+    data (){
+        return {
+             opcion:""
+        }
+    }
+}
+</script>
+
+<style scoped>
+.texto {
+    color:red;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 16px;
+}
+</style>
