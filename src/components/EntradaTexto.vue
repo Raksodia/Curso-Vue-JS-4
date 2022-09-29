939dffdc0a81103b6c2d4c6851265d334477a8fe
Williamns Robles <robles.williamns@gmail.com>
2022-09-28 19:17:06 -0500

    input - Entrada de Datos
    input - Entrada de Datos

---
+++ src/components/EntradaTexto.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="EntradaTexto"> 
+        Entrada Texto 
+        <input v-bind:value="EntradaTexto"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EntradaTexo',
+    data (){
+        return {
+            EntradaTexto:"*** Texto ***"
+        }
+    },
+}
+</script>
+
+<style scoped>
+.texto {
+    color:red;
+    
+    font-size: 16px;
+}
+</style>
