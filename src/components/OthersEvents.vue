7a536d3dc9a93e4d2e82e3b5647dc4f1d1533443
Williamns Robles <robles.williamns@gmail.com>
2022-09-28 21:36:20 -0500

    Handling Others Events
    Handling Others Events

---
+++ src/components/OthersEvents.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="OthersEvents"> 
+        Others Events {{estado}}
+        <input @keypress="estado='teclado'" @keypress.enter="estado='teclado enter'">
+        <a v-on:click="estado='click'" v-on:mouseover="estado='onmouseover'"  class="btn btn-sucess">Click</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OthersEvents',
+    data (){
+        return { 
+            estado:""
+        }
+    },
+}
+</script>
+
+<style scoped>
+.texto {
+    color:red;
+    font-size: 16px;
+}
+</style>
