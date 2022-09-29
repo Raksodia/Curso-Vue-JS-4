f5fcbbe5d7f13b496fcf71237699c846e80069c3
Williamns Robles <robles.williamns@gmail.com>
2022-09-28 19:09:02 -0500

    Opcion Button v-on methods parameters
    Opcion Button v-on methods parameters

--- src/components/OpcionButton.vue
+++ src/components/OpcionButton.vue
@@ -5,6 +5,12 @@
         <hr>
         <h3>Opcion 2 {{opcion2}}</h3>
         <button class="btn btn-primary" v-on:click="sumar">Opcion 2</button>
+        <hr>
+        <h3>Opcion 3 {{texto}}</h3>
+        <button class="btn btn-primary" v-on:click="seleccion(opcion3)">Opcion 3</button>
+        <hr>
+        <h3>Opcion 4 {{texto}}</h3>
+        <button class="btn btn-primary" v-on:click="seleccion(opcion4)">Opcion 4</button>
     </div>
 </template>
 
@@ -13,13 +19,19 @@ export default {
     name: 'OpcionButton',
     data (){
         return {
-             opcion1:"",
-             opcion2:""
+            opcion1:"",
+            opcion2:"",
+            opcion3:"---Opcion-3---",
+            opcion4:"---Opcion-4---",
+            texto:""
         }
     },
     methods:{
         sumar: function(){
             this.opcion2 = Math.floor(Math.random() * 100);
+        },
+        seleccion: function(opcion){
+            this.texto = opcion;
         }
     }
 }
