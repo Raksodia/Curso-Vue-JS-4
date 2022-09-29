6f9571de27e4a795c1d83513b2cba67061453fab
Williamns Robles <robles.williamns@gmail.com>
2022-09-28 20:56:33 -0500

    Two Way Binding
    Two Way Binding

---
+++ src/components/TwoWayBinding.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="TwoWayBinding"> 
+        Two Way Binding {{EntradaTexto}}
+        <input v-model="EntradaTexto"/>
+        <button class="btn btn-primary" v-on:click="EntradaTexto='Nuevo Texto'">Cambiar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TwoWayBinding',
+    data (){
+        return { 
+            EntradaTexto:"*** Texto ***"
+        }
+    },
+}
+</script>
+
+<style scoped>
+.texto {
+    color:red;
+    
+    font-size: 16px;
+}
+</style>
